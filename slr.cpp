4f56f30c27a933ec0912a9686e07487ebdd445cf
Bastien Saidi <saidi.ireland@gmail.com>
2021-02-23 08:37:42 +0000

    Save 1. wip: Xiaolin line theory implementation

--- slr.cpp
+++ slr.cpp
@@ -9,24 +9,28 @@
 #include <sstream>
 #include <string>
 
+#include <algorithm>
+#include <utility>
 
+//Function to convert a variable type in memory to std::string
 template <typename T> std::string tostr(const T& t) {
     std::ostringstream os;
     os << t;
     return os.str();
 }
 
-//Function for finding absolute value
 
-
-
+//Todo add a or expression in case 0
+//handler to alter the sorting function behaviour. sorts in order
 
 int sort_handler1(int a, int b) {
     return a - b;
 }
 
+//handler to alter the sorting function behaviour. sorts backward.
+
 int sort_handler2(int a, int b) {
-    return a - b;
+    return b - a;
 }
 
 // C++ port of JS's array.prototype.sort() function. Supports handler to change the sorting behaviour.
@@ -60,10 +64,11 @@ std::vector<int> sort(std::vector<int> p, int arrsize, std::function<int (int, i
 
 
 
-
+//handler function run in loop. it adds i to the total recursively.
 int reducehandler1(int total, int i) {
     return total + i;
 }
+
 // JS port of Reduce function from array prototype.reduce(). Supports handler to change the behaviour.
 int reduce(std::function<int (int, int)> handler, std::vector<int> iter, int arrsize, int initial = 0) {
 
@@ -81,7 +86,7 @@ int reduce(std::function<int (int, int)> handler, std::vector<int> iter, int arr
 
 
 
-
+//Round down a number like std::floor by converting the number to an int.
 int _ROUND(double a) {
     double b = a + 0.5;
     return (int)b;
@@ -112,6 +117,9 @@ std::vector<std::vector<int> > line(int X0, int Y0, int X1, int Y1)
     float X = X0;
     float Y = Y0;
     std::vector<std::vector<int> > points;
+
+    //TODO when other functions implemented.
+    //Check if steps is too low. if so then switch to next algorithm int* linehoriz
 
     for (int i = 0; i <= steps; i++)
     {
@@ -145,6 +153,126 @@ int abs (int n)
     return ( (n > 0) ? n : ( n * (-1)));
 }
 
+int _fpart(int x){
+    return x - std::floor(x);
+}
+
+int _rfpart(int x){
+    return 1 - _fpart(x);
+}
+
+
+std::vector<std::vector<int> > linehoriz (short X0, short Y0, short X1, short Y1,
+         short BaseColor, short NumLevels, unsigned short IntensityBits)
+{
+   unsigned short IntensityShift, ErrorAdj, ErrorAcc;
+   unsigned short ErrorAccTemp, Weighting, WeightingComplementMask;
+   short DeltaX, DeltaY, Temp, XDir;
+
+   /* Make sure the line runs top to bottom */
+   if (Y0 > Y1) {
+      Temp = Y0; Y0 = Y1; Y1 = Temp;
+      Temp = X0; X0 = X1; X1 = Temp;
+   }
+   /* Draw the initial pixel, which is always exactly intersected by
+      the line and so needs no weighting */
+   DrawPixel(X0, Y0, BaseColor);
+
+   if ((DeltaX = X1 - X0) >= 0) {
+      XDir = 1;
+   } else {
+      XDir = -1;
+      DeltaX = -DeltaX; /* make DeltaX positive */
+   }
+   /* Special-case horizontal, vertical, and diagonal lines, which
+      require no weighting because they go right through the center of
+      every pixel */
+   if ((DeltaY = Y1 - Y0) == 0) {
+      /* Horizontal line */
+      while (DeltaX-- != 0) {
+         X0 += XDir;
+         DrawPixel(pDC,X0, Y0, BaseColor);
+      }
+      return;
+   }
+   if (DeltaX == 0) {
+      /* Vertical line */
+      do {
+         Y0++;
+         DrawPixel(X0, Y0, BaseColor);
+      } while (--DeltaY != 0);
+      return;
+   }
+   if (DeltaX == DeltaY) {
+      /* Diagonal line */
+      do {
+         X0 += XDir;
+         Y0++;
+         DrawPixel(X0, Y0, BaseColor);
+      } while (--DeltaY != 0);
+      return;
+   }
+   /* Line is not horizontal, diagonal, or vertical */
+   ErrorAcc = 0;  /* initialize the line error accumulator to 0 */
+   /* # of bits by which to shift ErrorAcc to get intensity level */
+   IntensityShift = 16 - IntensityBits;
+   /* Mask used to flip all bits in an intensity weighting, producing the
+      result (1 - intensity weighting) */
+   WeightingComplementMask = NumLevels - 1;
+   /* Is this an X-major or Y-major line? */
+   if (DeltaY > DeltaX) {
+      /* Y-major line; calculate 16-bit fixed-point fractional part of a
+         pixel that X advances each time Y advances 1 pixel, truncating the
+         result so that we won't overrun the endpoint along the X axis */
+      ErrorAdj = ((unsigned long) DeltaX << 16) / (unsigned long) DeltaY;
+      /* Draw all pixels other than the first and last */
+      while (--DeltaY) {
+         ErrorAccTemp = ErrorAcc;   /* remember current accumulated error */
+         ErrorAcc += ErrorAdj;      /* calculate error for next pixel */
+         if (ErrorAcc <= ErrorAccTemp) {
+            /* The error accumulator turned over, so advance the X coord */
+            X0 += XDir;
+         }
+         Y0++; /* Y-major, so always advance Y */
+         /* The IntensityBits most significant bits of ErrorAcc give us the
+            intensity weighting for this pixel, and the complement of the
+            weighting for the paired pixel */
+         Weighting = ErrorAcc >> IntensityShift;
+         DrawPixel(pDC,X0, Y0, BaseColor + Weighting);
+         DrawPixel(pDC,X0 + XDir, Y0,
+               BaseColor + (Weighting ^ WeightingComplementMask));
+      }
+      /* Draw the final pixel, which is 
+         always exactly intersected by the line
+         and so needs no weighting */
+      DrawPixel(X1, Y1, BaseColor);
+      return;
+   }
+   /* It's an X-major line; calculate 16-bit fixed-point fractional part of a
+      pixel that Y advances each time X advances 1 pixel, truncating the
+      result to avoid overrunning the endpoint along the X axis */
+   ErrorAdj = ((unsigned long) DeltaY << 16) / (unsigned long) DeltaX;
+   /* Draw all pixels other than the first and last */
+   while (--DeltaX) {
+      ErrorAccTemp = ErrorAcc;   /* remember current accumulated error */
+      ErrorAcc += ErrorAdj;      /* calculate error for next pixel */
+      if (ErrorAcc <= ErrorAccTemp) {
+         /* The error accumulator turned over, so advance the Y coord */
+         Y0++;
+      }
+      X0 += XDir; /* X-major, so always advance X */
+      /* The IntensityBits most significant bits of ErrorAcc give us the
+         intensity weighting for this pixel, and the complement of the
+         weighting for the paired pixel */
+      Weighting = ErrorAcc >> IntensityShift;
+      DrawPixel(X0, Y0, BaseColor + Weighting);
+      DrawPixel(X0, Y0 + 1,
+            BaseColor + (Weighting ^ WeightingComplementMask));
+   }
+   /* Draw the final pixel, which is always exactly intersected by the line
+      and so needs no weighting */
+   DrawPixel(pDC,X1, Y1, BaseColor);
+}
 
 int main() {
 
